e3e9cab668e3b5ca1a714d996c92500e9eab9b41
Pilgun Nastya <annasteysha666@gmail.com>
2017-09-21 18:17:26 +0300

    comment out test files for now

--- test/controllers/admin/application_controller_test.rb
+++ test/controllers/admin/application_controller_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
-
-class Admin::ApplicationControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# require 'test_helper'
+#
+# class Admin::ApplicationControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
--- test/controllers/admin/posts_controller_test.rb
+++ test/controllers/admin/posts_controller_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
-
-class Admin::PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# require 'test_helper'
+#
+# class Admin::PostsControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
